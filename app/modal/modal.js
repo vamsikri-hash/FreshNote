243158107ec13706563ec951c2845d16ab7c3429
vamsi <vamsikrishna3415@outlook.in>
2020-05-22 22:47:15 +0530

    Added Delete , need to fix button

--- app/modal/modal.js
+++ app/modal/modal.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
         .context()
         .then(function (context) {
           showNotes(context.data);
+          $("#deleteNote").click(function () {
+            console.log("here del clicked");
+            deleteNotes(context.data);
+          });
         })
         .catch(function (error) {
           console.error(error);
@@ -27,11 +31,47 @@ function showNotes(ticket) {
     .get(ticketId)
     .then(function (data) {
       var html = "";
-      html = `<h3>${data.title}</h3><p>${data.description}</p><button class="btn btn-primary">Delete</button>`;
+      html = `<h3>${data.title}</h3><p>${data.description}</p>`;
       $("#modal").append(html);
       console.log(data);
     })
     .catch(function (error) {
       console.error(error);
+    });
+}
+
+/**
+ * Function to delete note drom db
+ */
+
+function deleteNotes(ticket) {
+  console.log("clicked");
+  var ticketId = ticket.id;
+  client.db
+    .delete(ticketId)
+    .then(function (data) {
+      showNotification("success", "Hey", "Successfully deleted note!");
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
+/**
+ * Show notifications using notification API
+ * @param {String} type
+ * @param {String}  title
+ * @param {String} message
+ */
+
+function showNotification(type, title, message) {
+  client.interface
+    .trigger("showNotify", {
+      type: `${type}`,
+      title: `${title}`,
+      message: `${message}`,
+    })
+    .catch(function (error) {
+      console.error("Notification error", error);
     });
 }
